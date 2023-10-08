dc55ab58bb4f48b142720ab4299de5aad91ea482
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-10-08 15:52:14 -0400

    :building_construction: added root layout and tabs layout

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="movie"
+        options={{
+          headerShown: false,
+          title: "Movies",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="movie" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tv"
+        options={{
+          headerShown: false,
+          title: "TV Series",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="tv" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="artists"
+        options={{
+          title: "People",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="people" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function RootLayot() {
+export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
