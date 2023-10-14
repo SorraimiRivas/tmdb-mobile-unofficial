6b9ada11f2a4ea071b5e97cc4b988b551b221c69
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-10-13 21:09:44 -0400

    :recycle: refactored CastCard to have the same code on Android and IOS. They now look the same.

--- src/components/common/CastCard.tsx
+++ src/components/common/CastCard.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Image, Platform, StyleSheet } from "react-native";
 import React from "react";
-import { imageParser } from "@/lib/utils";
+import { getInitials, imageParser } from "@/lib/utils";
 import { profileSize } from "@/api";
 
 type CastCardProps = {
@@ -10,15 +10,22 @@ type CastCardProps = {
 };
 const CastCard = ({ name, profile_path, character }: CastCardProps) => {
   const profileURL = imageParser(profile_path, profileSize.original);
+  const initials = getInitials(name);
 
-  return Platform.OS === "ios" ? (
-    <View className="my-4 items-center">
-      <Image
-        source={{ uri: profileURL }}
-        className="h-32 w-[85px] rounded-lg"
-        style={{ resizeMode: "contain" }}
-      />
-      <View className="flex flex-col mt-1 w-32">
+  return (
+    <View className="w-44">
+      {profileURL ? (
+        <Image
+          source={{ uri: profileURL }}
+          className="h-44 w-44 rounded-full "
+          style={{ resizeMode: "cover" }}
+        />
+      ) : (
+        <View className="h-44 w-44 items-center justify-center rounded-full bg-tertiary">
+          <Text className="text-6xl font-bold text-gray-600">{initials}</Text>
+        </View>
+      )}
+      <View className="mt-1 flex flex-col ">
         <Text
           className="text-center font-bold"
           ellipsizeMode="tail"
@@ -26,37 +33,7 @@ const CastCard = ({ name, profile_path, character }: CastCardProps) => {
         >
           {name}
         </Text>
-        <Text
-          className="text-center text-gray-600"
-          ellipsizeMode="tail"
-          numberOfLines={2}
-        >
-          {character}
-        </Text>
-      </View>
-    </View>
-  ) : (
-    <View className="mx-2">
-      <Image
-        source={{ uri: profileURL }}
-        className="h-44 w-44 rounded-full "
-        style={{ resizeMode: "contain" }}
-      />
-      <View className="flex flex-col mt-1 ">
-        <Text
-          className="text-center font-bold"
-          ellipsizeMode="tail"
-          numberOfLines={2}
-        >
-          {name}
-        </Text>
-        <Text
-          className="text-center text-gray-600"
-          ellipsizeMode="tail"
-          numberOfLines={2}
-        >
-          {character}
-        </Text>
+        <Text className="text-center text-gray-600">{character}</Text>
       </View>
     </View>
   );
