d425009b816282c592c922805a610eedd8c6e552
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-10-23 07:38:06 -0400

    :construction: added profile picture and social media links

--- src/app/(tabs)/person/[id].tsx
+++ src/app/(tabs)/person/[id].tsx
@@ -1,16 +1,42 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import useGetPeopleById from "@/hooks/useGetPeopleById";
+import { ScrollView } from "react-native-gesture-handler";
+import { Image } from "expo-image";
+import { imageParser } from "@/lib/utils";
+import { profileSize } from "@/api";
+import { Grid } from "react-native-animated-spinkit";
+import { blurhash } from "@/lib/constants";
+import SocialMediaSection from "@/components/SocialMediaSection";
 
 const Details = () => {
   const { id } = useLocalSearchParams();
-  const { data, loading, error } = useGetPeopleById(`person/${id}`);
+  const { data, loading, error } = useGetPeopleById(`person/${id}`, {
+    append_to_response: "external_ids",
+  });
 
-  return (
-    <View>
-      <Text>Details</Text>
+  const imageURL = imageParser(data?.profilePath, profileSize.original);
+
+  return loading ? (
+    <View className="flex-1 items-center justify-center">
+      <Grid size={50} color="black" />
     </View>
+  ) : (
+    <ScrollView className="flex-1">
+      <Stack.Screen options={{ title: data?.name }} />
+      <View className="mt-4 h-[500] items-center">
+        <Image
+          source={{ uri: imageURL }}
+          className="mb-4 h-[250] w-[170] rounded-md"
+          contentFit="cover"
+          transition={{ effect: "curl-down", duration: 1000 }}
+          placeholder={blurhash}
+          cachePolicy={"memory"}
+        />
+        <SocialMediaSection data={data?.externalIds!} />
+      </View>
+    </ScrollView>
   );
 };
 
