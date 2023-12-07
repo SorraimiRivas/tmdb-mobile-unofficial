04e0adc0b8fe5bbe5e7132d4d894f65deaad3bad
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-12-06 20:15:59 -0400

    :necktie: added login logic to profile screen when user has not logged in

--- src/app/(drawer)/profile.tsx
+++ src/app/(drawer)/profile.tsx
@@ -1,26 +1,29 @@
 import { DrawerToggleButton } from "@react-navigation/drawer";
 import { View, Text, FlatList } from "react-native";
 import { Image } from "expo-image";
-import React from "react";
 
 import CountryFlag from "react-native-country-flag";
 
 import useGetUserFavoriteMovies from "@/hooks/useGetUserFavoriteMovies";
+import { FormattedMovies, FormattedSeries } from "@/lib/types";
 import CommonCard from "@/components/common/CommonCard";
 import { useAppSelector } from "@/hooks/useRedux";
 import { imageParser } from "@/lib/utils";
 import { profileSize } from "@/api";
-import { FormattedMovies, FormattedSeries } from "@/lib/types";
+
 import { ScrollView } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 
 const Profile = () => {
-  const { account } = useAppSelector((state) => state.userSession);
+  const router = useRouter();
+  const { account, isLogged } = useAppSelector((state) => state.userSession);
   const { data: movieData } = useGetUserFavoriteMovies("movies");
   const { data: seriesData } = useGetUserFavoriteMovies("tv");
 
   const avatar = account?.avatar?.tmdb.avatar_path;
   const avatarURL = imageParser(avatar, profileSize.original);
-
+  const flagCode: string = account?.iso_3166_1!;
   const renderMovies = ({
     item,
   }: {
@@ -37,6 +40,12 @@ const Profile = () => {
     return <CommonCard {...item} type="tv" />;
   };
 
+  useEffect(() => {
+    if (!isLogged) {
+      router.replace("/login/");
+    }
+  }, []);
+
   return (
     <View className="flex-1">
       <View className="relative h-24 bg-primary">
@@ -44,53 +53,48 @@ const Profile = () => {
           <DrawerToggleButton tintColor="white" />
         </View>
       </View>
-      <View className="mt-4 items-center">
-        <Image source={{ uri: avatarURL }} className="h-40 w-40 rounded-full" />
-      </View>
-      <View className="mt-4 items-center pb-4">
-        <Text className="mb-4 text-xl font-bold">
-          {account?.name || account?.username}
-        </Text>
-        <CountryFlag isoCode={account?.iso_3166_1!} size={40} />
-      </View>
-      {/* Favorites Movies */}
-      <ScrollView className="mb-4">
-        <View className="mt-4">
-          <Text className="ml-4 text-xl font-bold">Favorite Movies</Text>
-          <FlatList
-            data={movieData}
-            horizontal
-            keyExtractor={(item, index) => `${item.id + index}`.toString()}
-            renderItem={renderMovies}
-            ListEmptyComponent={() => (
-              <View>
-                <Text>No items found</Text>
-              </View>
-            )}
-            contentContainerStyle={{ gap: 10 }}
-            ListHeaderComponent={<View className="mx-1" />}
-            ListFooterComponent={<View className="mx-1" />}
-          />
-        </View>
-        {/* Favorite Series */}
-        <View className="mt-4">
-          <Text className="ml-4 text-xl font-bold">Favorite Series</Text>
-          <FlatList
-            data={seriesData}
-            horizontal
-            keyExtractor={(item, index) => `${item.id + index}`.toString()}
-            renderItem={renderSeries}
-            ListEmptyComponent={() => (
-              <View>
-                <Text>No items found</Text>
-              </View>
-            )}
-            contentContainerStyle={{ gap: 10 }}
-            ListHeaderComponent={<View className="mx-1" />}
-            ListFooterComponent={<View className="mx-1" />}
-          />
-        </View>
-      </ScrollView>
+      {isLogged && (
+        <ScrollView className="mb-4" showsVerticalScrollIndicator={false}>
+          <View className="mt-4 items-center">
+            <Image
+              source={{ uri: avatarURL }}
+              className="h-40 w-40 rounded-full"
+            />
+          </View>
+          <View className="mt-4 items-center pb-4">
+            <Text className="mb-4 text-xl font-bold">
+              {account?.name || account?.username}
+            </Text>
+            <CountryFlag isoCode={flagCode} size={40} />
+          </View>
+          {/* Favorites Movies */}
+          <View className="mt-4">
+            <Text className="ml-4 text-xl font-bold">Favorite Movies</Text>
+            <FlatList
+              data={movieData}
+              horizontal
+              keyExtractor={(item, index) => `${item.id + index}`.toString()}
+              renderItem={renderMovies}
+              contentContainerStyle={{ gap: 10 }}
+              ListHeaderComponent={<View className="mx-1" />}
+              ListFooterComponent={<View className="mx-1" />}
+            />
+          </View>
+          {/* Favorite Series */}
+          <View className="mt-4">
+            <Text className="ml-4 text-xl font-bold">Favorite Series</Text>
+            <FlatList
+              data={seriesData}
+              horizontal
+              keyExtractor={(item, index) => `${item.id + index}`.toString()}
+              renderItem={renderSeries}
+              contentContainerStyle={{ gap: 10 }}
+              ListHeaderComponent={<View className="mx-1" />}
+              ListFooterComponent={<View className="mx-1" />}
+            />
+          </View>
+        </ScrollView>
+      )}
     </View>
   );
 };
